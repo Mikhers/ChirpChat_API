c4f294699e56c47df926ecf7491075aff7ea86c4
~mikhers <~miguel.morenovuniagustiniana.edu.co>
2023-05-25 00:06:21 -0500

    listado de publicaciones propias

--- API_ChirpChat/urls.py
+++ API_ChirpChat/urls.py
@@ -1,6 +1,6 @@
 from rest_framework import routers
 from django.urls import path
-from .views_custom import AuthViewSet, ListaAmigosViewSet, ListaChatViewSet, ListasolicitudViewSet
+from .views_custom import AuthViewSet, ListaAmigosViewSet, ListaChatViewSet, ListasolicitudViewSet, ListapublicacionesViewSet
 from .views import UsuarioViewSet, AmigosViewSet, solicitudViewSet, PublicacionViewSet, MensajesViewSet, ChatViewSet
 router = routers.DefaultRouter()
 
@@ -16,7 +16,7 @@ urlpatterns = router.urls
 urlpatterns += [
     path('login/', AuthViewSet.as_view(), name='login'),
     path('lista-amigos/<str:id>/', ListaAmigosViewSet.as_view(), name='lista-de-amigos'),
-    path('lista-publicaciones/<str:id>/', ListaAmigosViewSet.as_view(), name='lista-de-publicaciones'),
+    path('lista-publicaciones/<str:id>/', ListapublicacionesViewSet.as_view(), name='lista-de-publicaciones'),
     path('lista-solicitudes/<str:id>/', ListasolicitudViewSet.as_view(), name='lista-de-solicitudes'),
     path('lista-chats/<str:id>/', ListaChatViewSet.as_view(), name='lista-de-chats'),
 ]
